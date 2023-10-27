1eda0496430b85072334dd00bdd0cd52adb3e398
Swati Yadawar <r.swati2211@gmail.com>
2023-10-27 20:01:58 +0530

    day2

---
+++ lib/home_dart.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final int days = 30;
+  final String name = "Codepur";
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      appBar:AppBar(
+     title: Text("Catlog App"),
+    ),
+        body: Center(
+      child: Container(
+        child: Text("welcome to $days days flutter challenge"),
+      ),
+    ),
+    drawer :Drawer(),
+    );
+  }
+}
